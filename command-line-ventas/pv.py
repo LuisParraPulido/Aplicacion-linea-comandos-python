c723aa6560d2745403e3f567633cef7d0090eb58
luis_parra <luis.parra.pulido@gmail.com>
2020-03-11 11:56:26 -0500

    click

---
+++ command-line-ventas/pv.py
@@ -0,0 +1,15 @@
+import click
+
+from clients import commands as clients_commmands
+
+# CLIENTS_TABLE = '.clients.csv'
+
+@click.group()
+@click.pass_context
+def cli(ctx):
+    """An application to manage clients, inventory sales and produce reports."""
+    ctx.obj = {}
+    # ctx.obj['clients_table'] = CLIENTS_TABLE
+
+
+cli.add_commands(clients_commmands.all)
